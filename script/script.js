45154023644dd9d37756c2ed2862f367b81482f0
Rhaegar259 <aleksandrvorushilo@gmail.com>
2021-07-30 15:44:04 +0300

    Add alert and log

--- script/script.js
+++ script/script.js
@@ -1,8 +1,12 @@
+
 var money = 100;
 let income = 150;
 let addExpenses;
 let deposit = 2;
 let mission = 500;
 const period = 360;
+
+alert('Привет Мир!');
+console.log('money: ', money);
 
 // income, addExpenses, deposit, mission, period
