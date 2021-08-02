fe3e846c27dd54f32802af79a444338563da2efe
Rhaegar259 <aleksandrvorushilo@gmail.com>
2021-08-02 18:49:32 +0300

    Mandatory task

--- script/script.js
+++ script/script.js
@@ -1,12 +1,26 @@
 
-var money = 100;
-let income = 150;
-let addExpenses;
-let deposit = 2;
-let mission = 500;
-const period = 360;
+let money;
 
-alert('Привет Мир!');
+money = 260;
 console.log('money: ', money);
+
+let income;
+income = 'freelance';
+console.log('income: ', income);
+let addExpenses;
+addExpenses = 'Food, communal apartment, fuel, entertainment';
+let deposit = true;
+let mission = 1000;
+const period = 6;
+console.log('deposit: ', deposit);
+console.log('Длинна строки - addExpenses: ',addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей/долларов/гривен/юани`);
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay;
+budgetDay = money/30;
+console.log('budgetDay: ', budgetDay);
+// alert('Привет Мир!');
 
 // income, addExpenses, deposit, mission, period
